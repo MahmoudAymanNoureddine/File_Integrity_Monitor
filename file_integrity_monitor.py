a424e46f7cb4db61bbfefae078a387e26f3c5e82
Mahmoud Ayman Noureddine <memonour2002@outlook.com>
2026-08-27 17:35:34 +0300

    Update file_integrity_monitor.py

--- file_integrity_monitor.py
+++ file_integrity_monitor.py
@@ -6,6 +6,8 @@ files_to_monitor = [
     "security.log"
 ]
 
+current_hashes = []
+
 print("FILE INTEGRITY MONITOR")
 print("=" * 50)
 
@@ -17,7 +19,18 @@ for file_name in files_to_monitor:
 
     file_hash = hashlib.sha256(data).hexdigest()
 
+    current_hashes.append(f"{file_name}:{file_hash}")
+
     print(f"\nFile: {file_name}")
     print(f"SHA256: {file_hash}")
 
     file.close()
+
+baseline_file = open("baseline_hashes.txt", "w")
+
+for item in current_hashes:
+    baseline_file.write(item + "\n")
+
+baseline_file.close()
+
+print("\nBaseline Hashes Saved Successfully.")
