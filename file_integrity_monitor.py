98aea5bd60c3e493acaf92a807a031f75b46d1fa
Mahmoud Ayman Noureddine <memonour2002@outlook.com>
2026-08-27 17:38:22 +0300

    Update file_integrity_monitor.py

--- file_integrity_monitor.py
+++ file_integrity_monitor.py
@@ -6,10 +6,20 @@ files_to_monitor = [
     "security.log"
 ]
 
-current_hashes = []
-
 print("FILE INTEGRITY MONITOR")
-print("=" * 50)
+print("=" * 60)
+
+baseline_hashes = {}
+
+baseline_file = open("baseline_hashes.txt", "r")
+
+for line in baseline_file:
+    file_name, file_hash = line.strip().split(":")
+    baseline_hashes[file_name] = file_hash
+
+baseline_file.close()
+
+modified_files = 0
 
 for file_name in files_to_monitor:
 
@@ -17,20 +27,30 @@ for file_name in files_to_monitor:
 
     data = file.read()
 
-    file_hash = hashlib.sha256(data).hexdigest()
-
-    current_hashes.append(f"{file_name}:{file_hash}")
-
-    print(f"\nFile: {file_name}")
-    print(f"SHA256: {file_hash}")
+    current_hash = hashlib.sha256(data).hexdigest()
 
     file.close()
 
-baseline_file = open("baseline_hashes.txt", "w")
+    print(f"\nFile: {file_name}")
 
-for item in current_hashes:
-    baseline_file.write(item + "\n")
+    if current_hash == baseline_hashesprint("Status : SAFE")
 
-baseline_file.close()
+    else:
+        print("Status : MODIFIED")
+        modified_files += 1
 
-print("\nBaseline Hashes Saved Successfully.")
+print("\n" + "=" * 60)
+
+print("Files Checked :", len(files_to_monitor))
+print("Modified Files:", modified_files)
+
+print("=" * 60)
+
+if modified_files == 0:
+    print("Risk Level : LOW")
+
+elif modified_files == 1:
+    print("Risk Level : MEDIUM")
+
+else:
+    print("Risk Level : HIGH")
